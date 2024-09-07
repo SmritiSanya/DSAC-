600024659e12302a4dadc03102f2909fadb9ad82
SmritiSanya <smriti.sanya@gmail.com>
2024-09-07 13:29:06 +0530

    update

---
+++ DSA C++/75_hashmaps.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //creation
+    unordered_map<string,int>m;
+
+    //insertion
+
+    //1
+    pair<string,int>p =make_pair("smriti",3);
+    m.insert(p);
+
+    //2
+    pair<string,int>pair2("sanya",2);
+    m.insert(pair2);
+
+    //3
+    m["mera"]=1;
+    m["mera"]=2;
+
+    //Search
+    cout<<m["mera"]<<endl;
+    cout<<m.at("sanya")<<endl;
+
+
+
+}
